e1041f101ee1dd8a4472272426fec39e65eb087f
yacine <yacine@thinkpad>
2024-07-17 14:11:59 +0200

    initial commit

---
+++ scripts/replace.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+audit_key = sys.argv[1].replace("&", "&amp;")#.removeprefix("QR-Code:")
+spend_key = sys.argv[2].replace("&", "&amp;")#.removeprefix("QR-Code:")
+
+
+with open("tmp/content.xml") as f:
+    text = f.read()
+
+text = text.replace("audit-url", audit_key)
+text = text.replace("spend-url", spend_key)
+
+with open("tmp/content.xml", "w") as f:
+    f.write(text)
+
+# return processed key strings for qrencode
+print(audit_key)
+print(spend_key)
